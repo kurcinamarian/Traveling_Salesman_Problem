e1e70aa9db309bba8b90991fd4076e52f3a56ecc
kurcinamarian <marian.kurcina2003@gmail.com>
2025-02-03 16:03:26 +0100

    translation of 1cC

--- 1cC.py
+++ 1cC.py
@@ -6,9 +6,9 @@ tk = tkinter.Tk()
 canvas = tkinter.Canvas(tk, width=500, height=500)
 canvas.pack()
 
-#ulozenie dat pre mesta####################################################
+#Storing data for cities####################################################
 
-number_of_cities = int(input("Pre kolko miest chces najst najlepsiu cestu: "))
+number_of_cities = int(input("For how many cities do you want to find the best route: "))
 '''
 cities_x = [290, 100, 380, 450, 70, 330, 170, 150, 150, 170, 230, 390, 350, 480, 40, 430, 310, 330, 100, 180,
 60, 120, 70, 80, 400, 420, 170, 290, 140, 430, 240, 400, 440, 100, 180, 100, 30, 110, 140, 320]
@@ -115,22 +115,22 @@ def search(temp,lowest_temp,rate):
         temp = rate*temp
         l += 1
         draw(current_path)
-    print("Najlepsia cesta je:", current_path)
-    print("Jej dlzka je:", current_distance)
-    print("Pocet iteracii:", l)
+    print("The best route is:", current_path)
+    print("Its length is:", current_distance)
+    print("Number of iterations:", l)
     return current_distance
 
-#start hladania
+#Start the search
 s = 0
-test = input("Chces testovat? (ano/nie): ")
-if test == "nie":
+test = input("Do you want to test? (yes/no): ")
+if test == "no":
     search(55.48,1,0.992)
 else:
-    t = float(input("Aka ma byt pociatocna teplota: "))
-    r = float(input("Aka ma byt  miera ochladzovania: "))
-    m = float(input("Aka ma byt minimalna teplota: "))
-    num = int(input("Zadaj pocet vypoctov: "))
+    t = float(input("What should the initial temperature be: "))
+    r = float(input("What should the cooling rate be: "))
+    m = float(input("What should the minimum temperature be: "))
+    num = int(input("Enter the number of calculations: "))
     for i in range(num):
         s +=  search(t,m,r)
-    print("Priemerne dlha cesta je:", s/num)
+    print("The average route length is:", s/num)
 tk.mainloop()
